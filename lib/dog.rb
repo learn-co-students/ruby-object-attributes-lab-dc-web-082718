bd09d599638374f5b9c22373cde7002f04bcdf0e
kruchkamgar <kruchkamgar@gmail.com>
2018-08-28 17:50:14 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+  def name=(my_name)
+    name = my_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(my_breed)
+    breed = my_breed
+  end
+
+  def breed
+    @breed
+  end
+
+
+end
